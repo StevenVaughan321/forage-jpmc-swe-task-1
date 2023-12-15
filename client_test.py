3623b09fa684f6a28863e9da2d01ca17d7a9499f
Steven Vaughan <stvenvaughan@gmail.com>
2023-12-15 14:31:23 -0500

    Fixed getDataPoint in client3.py, added tests

--- client_test.py
+++ client_test.py
@@ -7,14 +7,28 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
+
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      stock, bid_price, ask_price, price = getDataPoint(quote)
+
+      # Assert that the calculated price is the average of bid and ask prices
+      expected_price = (ask_price + bid_price) / 2
+      self.assertEqual(price, expected_price)
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
       {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
+
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      stock, bid_price, ask_price, price = getDataPoint(quote)
+
+      # Assertions
+      self.assertTrue(bid_price > ask_price)  # Bid price should be greater than ask price
+      self.assertEqual(price, (bid_price + ask_price) / 2)  # Calculated price should be the average of bid and ask prices
 
 
   """ ------------ Add more unit tests ------------ """
